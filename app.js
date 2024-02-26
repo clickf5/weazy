1bda0e8fa0273c384d82f17e6e6f60ba628d1ec8
Eugene Panin <paninhome@gmail.com>
2024-02-27 00:54:19 +0300

    add basic function

--- app.js
+++ app.js
@@ -1,23 +1,74 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { printSuccess, printHelp } from './services/log.service.js';
+import {printHelp, printSuccess, printError, printWeather} from './services/log.service.js';
+import {saveKeyValue, KEYS, getKeyValue} from './services/storage.service.js';
+import {getWeather} from "./services/api.service.js";
+import dedent from "dedent-js";
+
+const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Не передан токен');
+        return;
+    }
+    try {
+        await saveKeyValue(KEYS.token, token);
+        printSuccess('Токен сохранен')
+    } catch (e) {
+        printError(e.message);
+    }
+}
+
+const saveCity = async (city) => {
+    if (!city.length) {
+        printError('Не передан город');
+        return;
+    }
+
+    try {
+        await saveKeyValue(KEYS.city, city);
+        printSuccess('Город сохранен');
+    } catch (e) {
+        printError(e.message);
+    }
+}
+
+const getForcast = async () => {
+    try {
+        const city = process.env.CITY ?? await getKeyValue(KEYS.city);
+
+        if (!city) {
+            throw new Error('Не задан город. Задайте его через команду -c [CITY]');
+        }
+
+        const res = await getWeather(city);
+        printWeather(res);
+    } catch (e) {
+        if (e?.response?.status === 404) {
+            printError('Неверно указан город');
+        } else if (e?.response?.status === 401) {
+            printError('Неверно указан токен');
+        } else {
+            printError(e.message);
+        }
+    }
+
+}
 
 const initCLI = () => {
     const args = getArgs(process.argv);
     if (args.h) {
-        printHelp();
+        return printHelp();
     }
 
     if (args.t) {
-        // save token
+        return saveToken(args.t);
     }
 
-    if (args.s) {
-        // save city
+    if (args.c) {
+        return saveCity(args.c);
     }
 
-    // print weather
-
+    return getForcast();
 };
 
 initCLI();
