55e0dbfbf92ed37fb182451adb6cb7d33bdf2186
Eugene Panin <paninhome@gmail.com>
2024-02-27 00:54:01 +0300

    add print weather method

--- services/log.service.js
+++ services/log.service.js
@@ -1,5 +1,6 @@
 import chalk from 'chalk';
 import dedent from 'dedent-js';
+import {getIcon} from "./api.service.js";
 const printError = (error) => {
     console.log(chalk.bgRed(' ERROR ') + ' ' + error);
 };
@@ -17,4 +18,14 @@ const printHelp = () => {
     );
 }
 
-export { printError, printSuccess, printHelp };
+const printWeather = (res) => {
+    console.log(
+        dedent`${chalk.bgYellow(' WEATHER ')} Погода в городе ${res.name}
+        ${getIcon(res.weather[0].icon)} ${res.weather[0].description}
+        Температура: ${res.main.temp}\u00b0C (ощущается как ${res.main.feels_like}\u00b0C)
+        Влажность: ${res.main.humidity}%
+        Скорость ветра: ${res.wind.speed} м/с`
+    );
+}
+
+export { printError, printSuccess, printHelp, printWeather };
